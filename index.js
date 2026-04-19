fc3d741e9448f9b490eacdea698a77c7966e9d0d
shakil <shakil.neub@gmail.com>
2026-04-20 01:34:50 +0600

    html file render

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import express from 'express';
-import { add } from './math.js';
-
+import express from 'express'; 
+import path from 'path';
 
 const app = express(); // Create an instance of the Express application
 const port = 3000; // Define the port number for the server to listen on
@@ -9,15 +8,13 @@ const log = console.log;
 
 // Define a route for the root URL ("/")
 app.get('/', (req, res) => {
-    log(add(5, 3)); // Log the result of adding 5 and 3 using the add function from math.js
-    res.send('Hello World!'); // Send a response to the client
+
+    const filePath = path.resolve('index.html'); // Construct the file path to index.html
+
+     res.sendFile(filePath); // Send the HTML file as a response to the client
 });
 
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About Us</h1><p>This is the about page.</p>'); // Send a response to the client for the about page
-});
-
+ 
 
 app.listen(port, () => {
     log(`Example app listening at http://localhost:${port}`); // Log a message when the server starts
